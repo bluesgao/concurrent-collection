cec0b31e36bce7492f6ad9578e284fd864133a50
gaoxin11 <gaoxin11@jd.com>
2019-04-16 14:50:28 +0800

    cowlist test

--- cowlist.go
+++ cowlist.go
@@ -2,43 +2,35 @@ package cowlist
 
 import (
 	"errors"
-	"strings"
+	"log"
 	"sync"
 )
 
 type element = interface{}
+
+const DEFAULT_CAPACITY = int(8)
+
+const INT_MAX = int(^uint(0) >> 1) //int最大值
 
 type CopyOnWriteList struct {
 	mutex    sync.Mutex //互斥锁
 	elements []element  //存放数据的数组
 }
 
-func New() *CopyOnWriteList {
-	return new(CopyOnWriteList).Init()
+func New(capacity int) *CopyOnWriteList {
+	log.Printf("INT_MAX:%d \n", INT_MAX)
+	if capacity <= 0 || capacity > INT_MAX {
+		capacity = DEFAULT_CAPACITY
+	}
+	return new(CopyOnWriteList).Init(capacity)
 }
 
-func (cowlist *CopyOnWriteList) Init() *CopyOnWriteList {
-	cowlist.elements = make([]element, 8, 16)
+func (cowlist *CopyOnWriteList) Init(capacity int) *CopyOnWriteList {
+	cowlist.elements = make([]element, 0, capacity)
 	return cowlist
 }
 
-func (cowlist *CopyOnWriteList) ToString() string {
-	eles := cowlist.elements //副本
-	len := len(eles)
-	if len == 0 {
-		return "[]"
-	}
-
-	var str strings.Builder
-	str.WriteString("[")
-	for i := 0; i < len; i++ {
-		//str.WriteString(fmt)
-		//todo
-	}
-	str.WriteString("]")
-	return str.String()
-}
-
+/*非线程安全*/
 func (cowlist *CopyOnWriteList) Get(index int) element {
 	eles := cowlist.elements //副本
 	return eles[index]
@@ -76,40 +68,68 @@ func (cowlist *CopyOnWriteList) AddByPosition(index int, ele element) (bool, err
 
 func (cowlist *CopyOnWriteList) Remove(index int) (element, error) {
 	cowlist.mutex.Lock()
-	len := len(cowlist.elements)
+	oldLen := len(cowlist.elements)
 	//index不在有效范围内
-	if index > len || index < 0 {
+	if index >= oldLen || index < 0 {
 		return nil, errors.New("index out of bounds")
 	}
 	//被删除元素
 	removedElement := cowlist.Get(index)
-	numMoved := len - index - 1
-	new1 := cowlist.elements[0:numMoved]
-	new2 := cowlist.elements[numMoved+1 : len]
-	newElements := append(new1, new2)
-	cowlist.elements = newElements
-
+	//log.Printf("removedElement:%v \n", removedElement)
+	//第一个元素
+	if index == 0 {
+		cowlist.elements = cowlist.elements[index+1 : oldLen]
+	} else if index == oldLen-1 { //最后一个元素
+		cowlist.elements = cowlist.elements[0 : oldLen-1]
+	} else { //中间元素
+		var newElements []element
+		//将原始数组按照index分割成两部分
+		part1 := cowlist.elements[0 : index-1]
+		part2 := cowlist.elements[index:oldLen]
+		//将分割后的两部分连接起来
+		for i := 0; i < len(part1); i++ {
+			newElements = append(newElements, part1[i])
+		}
+		for i := 0; i < len(part2); i++ {
+			newElements = append(newElements, part2[i])
+		}
+		cowlist.elements = newElements
+	}
 	defer cowlist.mutex.Unlock()
 	return removedElement, nil
 }
 
 func (cowlist *CopyOnWriteList) RemoveRange(fromIndex int, toIndex int) (bool, error) {
 	cowlist.mutex.Lock()
-	len := len(cowlist.elements)
+	oldLen := len(cowlist.elements)
 	//fromIndex与toIndex不在有效范围内
-	if fromIndex < 0 || toIndex > len || toIndex < fromIndex {
+	if fromIndex < 0 || toIndex >= oldLen || toIndex < fromIndex {
 		return false, errors.New("index out of bounds")
 	}
 
-	newLen := len - (toIndex - fromIndex)
-	numMoved := len - toIndex
-	//删除到数组尾部
-	if numMoved == 0 {
-		cowlist.elements = cowlist.elements[0:newLen]
-	} else {
-		new1 := cowlist.elements[0:fromIndex]
-		new2 := cowlist.elements[toIndex:newLen]
-		newElements := append(new1, new2)
+	//从第一个元素开始删除几个元素
+	if fromIndex == 0 && toIndex >= 0 {
+		cowlist.elements = cowlist.elements[toIndex+1 : oldLen]
+	} else if fromIndex >= 0 && toIndex == oldLen-1 { //删除最后几个元素
+		cowlist.elements = cowlist.elements[0:fromIndex]
+	} else { //删除中间几个元素
+		var newElements, part1, part2 []element
+		//将原始数组按照rangeindex分割成两部分
+		if fromIndex == toIndex {
+			part1 = cowlist.elements[0 : fromIndex-1]
+			part2 = cowlist.elements[fromIndex:oldLen]
+		} else {
+			part1 = cowlist.elements[0 : fromIndex-1]
+			part2 = cowlist.elements[toIndex+1 : oldLen]
+		}
+
+		//将分割后的两部分连接起来
+		for i := 0; i < len(part1); i++ {
+			newElements = append(newElements, part1[i])
+		}
+		for i := 0; i < len(part2); i++ {
+			newElements = append(newElements, part2[i])
+		}
 		cowlist.elements = newElements
 	}
 
@@ -123,10 +143,12 @@ func (cowlist *CopyOnWriteList) Clear() {
 	defer cowlist.mutex.Unlock()
 }
 
+/*非线程安全*/
 func (cowlist *CopyOnWriteList) Size() int {
 	return len(cowlist.elements)
 }
 
+/*非线程安全*/
 func (cowlist *CopyOnWriteList) IsEmpty() bool {
 	return len(cowlist.elements) == 0
 }
---
+++ cowlist_test.go
@@ -0,0 +1,84 @@
+package cowlist
+
+import (
+	"log"
+	"runtime"
+	"strconv"
+	"sync"
+	"testing"
+)
+
+var MAXPROCS int
+
+func init() {
+	MAXPROCS = runtime.NumCPU()
+}
+
+func TestCopyOnWriteList_Add(t *testing.T) {
+	list := New(4)
+	list.Add("1234")
+	t.Logf("list: %v \n", list.elements)
+}
+
+func TestCopyOnWriteList_Add2(t *testing.T) {
+	cowl := New(4)
+	wg := new(sync.WaitGroup)
+	wg.Add(MAXPROCS + 1)
+	for i := 0; i < MAXPROCS+1; i++ {
+		go func() {
+			for j := 0; j < 1000; j++ {
+				ele := strconv.Itoa(i) + ":" + strconv.Itoa(j)
+				cowl.Add(ele)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	log.Printf("MAXPROCS:%d,Size:%d,cowl:%v \n", MAXPROCS, cowl.Size(), cowl)
+}
+
+func TestCopyOnWriteList_Remove(t *testing.T) {
+	cowl := New(4)
+	//init data
+	for i := 0; i < 10; i++ {
+		cowl.Add("ele" + strconv.Itoa(i))
+	}
+	ele, err := cowl.Remove(9)
+	log.Printf("removedElement:%v ,Err:%v,Size:%d,elements:%v \n", ele, err, cowl.Size(), cowl.elements)
+}
+
+func TestCopyOnWriteList_Remove2(t *testing.T) {
+	cowl := New(4)
+	//init data
+	for i := 0; i < 10; i++ {
+		cowl.Add("ele" + strconv.Itoa(i))
+	}
+
+	wg := new(sync.WaitGroup)
+	wg.Add(4)
+	for j := 0; j < 4; j++ {
+		go func(index int) {
+			log.Printf("Before removedIndex:%d, Size:%d,elements:%v \n", index, cowl.Size(), cowl.elements)
+			ele, err := cowl.Remove(index)
+			log.Printf("After removedIndex:%d, removedElement:%v ,Err:%v,Size:%d,elements:%v \n", index, ele, err, cowl.Size(), cowl.elements)
+			//time.Sleep(time.Second * 2)
+			wg.Done()
+		}(j)
+	}
+	wg.Wait()
+	log.Printf("Size:%d,elements:%v \n", cowl.Size(), cowl.elements)
+}
+
+func TestCopyOnWriteList_RemoveRange(t *testing.T) {
+	cowl := New(4)
+	//init data
+	for i := 0; i < 20; i++ {
+		cowl.Add("ele" + strconv.Itoa(i))
+	}
+
+	log.Printf("remove before Size:%d,elements:%v \n", cowl.Size(), cowl.elements)
+	ret,err := cowl.RemoveRange(0, 1)
+	log.Printf("remove after Ret:%v ,Err:%v,Size:%d,elements:%v \n", ret, err, cowl.Size(), cowl.elements)
+}
+
+
